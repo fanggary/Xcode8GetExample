812f8cfdbb4166b6f4285afe21af8287f0a43f9c
Gary Fang <gary@garypersonal.local>
2017-07-18 09:32:40 +0800

    Just added a comment

--- Xcode8GitExample/ViewController.swift
+++ Xcode8GitExample/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // additional comments
     }
 
     override var representedObject: Any? {
